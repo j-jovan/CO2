423f6c6666e78fcb2cadce752472ac948b23ade4
j-jovan <abc.j.jovan@gmail.com>
2020-04-14 19:21:46 +0200

    Improved SCD30 code

--- SCD30/SCD30.ino
+++ SCD30/SCD30.ino
@@ -3,15 +3,19 @@
 #include "SparkFun_SCD30_Arduino_Library.h" //Biblioteka: http://librarymanager/All#SparkFun_SCD30
 SCD30 airSensor;
 
+
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("SCD30");
   Wire.begin();
 
+
+
   if (airSensor.begin() == false)
   {
-    Serial.println("SCD30 nije povezan");
+    Serial.println("Problem sa programom");
     while (1)
       ;
   }
@@ -21,20 +25,36 @@ void setup()
 void loop()
 {
   if (airSensor.dataAvailable())
+
   {
-    Serial.print("co2(ppm):");
-    Serial.print(airSensor.getCO2());
+    int co2 = airSensor.getCO2();
+    int temp = airSensor.getTemperature() ;
+    int vlaznost = airSensor.getHumidity();
+    
+    Serial.print("CO2(ppm): ");
+    Serial.print(co2);
 
-    Serial.print(" temp(C):");
-    Serial.print(airSensor.getTemperature(), 1);
+    Serial.print(" temp(C): ");
+    Serial.print(temp, 1);
 
-    Serial.print(" humidity(%):");
-    Serial.print(airSensor.getHumidity(), 1);
-
-    Serial.println();
+    Serial.print(" humidity(%): ");
+    Serial.print(vlaznost, 1);
+    Serial.println(" ");
+    if(co2 > 250 and co2 < 400)
+      Serial.println("Normalna koncentracija CO2");
+          if(co2 > 400 and co2 < 1000)
+      Serial.println("Ocekivana koncentracija u zatvorenom prostoru sa dobrim protokom vazduha");
+          if(co2 > 1000 and co2 < 2000)
+      Serial.println("Manjak koncentracije, pospanost i los kvalitet vazduha");
+          if(co2 > 2000 and co2 < 5000)
+      Serial.println("Glavobolja, pospanost, zagadjen vazduh, losa koncentracija, gubitak paznje i ubrzan rad srca");
+          if(co2 > 5000)
+      Serial.println("How are you reading this? And why?");
+          if(co2 > 40000)
+      Serial.println("RIP");
   }
   else
-    Serial.println("Cekam podatke");
+    //Serial.println("Cekam podatke");
 
   delay(500);
 }
